fea490e249594518e47d597590df3956086de8c2
shoaib143-sudo <shoaib.akram@outlook.com>
2026-09-11 07:41:31 +0800

    Add fail-closed OPA Render startup

---
+++ infra/opa/render-start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+: "${PORT:?Render PORT is required}"
+: "${OPA_AUTH_TOKEN:?OPA_AUTH_TOKEN is required}"
+
+test -x .render/opa/opa
+test -s .render/opa/bundle.tar.gz
+
+exec .render/opa/opa run \
+  --server \
+  --bundle .render/opa/bundle.tar.gz \
+  --config-file infra/opa/opa-config.yaml \
+  --authentication=token \
+  --authorization=basic \
+  --addr "0.0.0.0:${PORT}" \
+  --disable-telemetry
